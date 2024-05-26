1adac73cb20225a53f9835be9cd98db3612cf786
group04 <dissanayakebackup000@gmail.com>
2024-05-26 14:07:23 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var mySwiper = new Swiper('.swiper', {
+        
+        loop: true, // Enable looping
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 5000, // Set autoplay delay to 3 seconds (3000 milliseconds)
+            disableOnInteraction: false, // Continue autoplay even when user interacts with swiper
+        },
+    });
+  });
+  
